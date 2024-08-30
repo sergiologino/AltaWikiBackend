a6683ba45bd4230abe3f9b05b2c3544c0a2e3b47
sergiologino <savkin.sergey@yandex.ru>
2024-08-30 14:43:35 +0300

    working, but must review dto for spaces

---
+++ src/main/java/com/example/wikibackend/dto/UserDTO.java
@@ -0,0 +1,43 @@
+package com.example.wikibackend.dto;
+
+public class UserDTO {
+
+    private String username;
+    private String password;
+    private String email;
+
+    // Конструкторы, геттеры и сеттеры
+
+    public UserDTO() {}
+
+    public UserDTO(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
