c0d4451418093192938c2790ac18a66d8fe683b8
sergiologino <savkin-sergey@yandex.ru>
2024-09-29 18:38:45 +0300

    added docker files

--- src/main/resources/admin_schema.sql
+++ src/main/resources/admin_schema.sql
@@ -7,9 +7,8 @@ CREATE TABLE admin.organizations (
                                      name VARCHAR(255) NOT NULL,
                                      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE IF NOT EXISTS admin.users_admin (
+CREATE TABLE IF NOT EXISTS admin.users_organization (
                                                  user_id UUID PRIMARY KEY,
                                                  organization_id UUID NOT NULL REFERENCES admin.organizations(id),
-                                                 username VARCHAR(255) NOT NULL,
-                                                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                                 username VARCHAR(255) NOT NULL
 );
