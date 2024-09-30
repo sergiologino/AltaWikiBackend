5e06a6afa2d228d2342b42d4c643af069f32dfe4
sergiologino <savkin-sergey@yandex.ru>
2024-09-30 23:01:42 +0300

    fix security config cors

--- src/main/java/com/example/wikibackend/config/SecurityConfig.java
+++ src/main/java/com/example/wikibackend/config/SecurityConfig.java
@@ -13,9 +13,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -23,19 +30,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        // Разрешить доступ к Swagger и публичным ресурсам
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        // Разрешить доступ к эндпойнтам регистрации и авторизации пользователей
-                        .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/api/users/**").permitAll()
-                        // Все остальные запросы требуют аутентификации
-                        .anyRequest().authenticated()
-                )
-                .csrf(AbstractHttpConfigurer::disable).cors();
-        return http.build();
+            http.csrf(AbstractHttpConfigurer::disable)
+                    .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
+                    .authorizeHttpRequests((requests) -> requests
+                            .requestMatchers("/api/users", "/api/roles").authenticated()
+                            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                    );
+            http.formLogin(withDefaults());
+            http.httpBasic(withDefaults());
+            return http.build();
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        // Разрешить доступ к Swagger и публичным ресурсам
+//                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+//                        // Разрешить доступ к эндпойнтам регистрации и авторизации пользователей
+//                        .requestMatchers("/api/**").permitAll()
+//                        .requestMatchers("/api/users/**").permitAll()
+//                        // Все остальные запросы требуют аутентификации
+//                        .anyRequest().authenticated()
+//                )
+//                .csrf(AbstractHttpConfigurer::disable).cors();
+//        return http.build();
     }
 
     @Bean
@@ -55,19 +71,34 @@ public class SecurityConfig {
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-                //WebMvcConfigurer.super.addCorsMappings(registry);
-            }
-        };
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //Make the below setting as * to allow connection from any hos
+        corsConfiguration.setAllowedOrigins(List.of("/**"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST","PUT","DELETE","OPTIONS"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+//                        .allowedHeaders("*")
+//                        .exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+//                        .allowCredentials(true);
+//                //WebMvcConfigurer.super.addCorsMappings(registry);
+//            }
+//        };
+//    }
 
 
 }
