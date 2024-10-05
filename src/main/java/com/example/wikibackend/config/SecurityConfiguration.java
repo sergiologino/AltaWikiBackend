e3539012798350b6c3556bc353887804de7937c8
sergiologino <savkin-sergey@yandex.ru>
2024-10-05 20:50:22 +0300

    fix new config security

--- src/main/java/com/example/wikibackend/config/SecurityConfiguration.java
+++ src/main/java/com/example/wikibackend/config/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,6 +22,10 @@ public class SecurityConfiguration  {
                 )
                 .httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
